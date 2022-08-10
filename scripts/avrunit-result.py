00c5a8fef33aa2d10261ebc7f9df6260832bcc3c
Khairulmizam Samsudin <xource@gmail.com>
2022-08-10 10:07:02 +0800

    Use new build and test unit configuration

--- scripts/avrunit-result.py
+++ scripts/avrunit-result.py
@@ -1,59 +1,83 @@
 #!/usr/bin/python3
 # avrunit-result.py Copyright (c) 2022 Khairulmizam Samsudin <xource@gmail.com>
 #
-# Unpack avrunit result from AVR eeprom dump
+# Unpack avrunit result from AVR eeprom or serial dump
 #
 
 import sys
 import struct
+import argparse
+import serial
+import time
 
+AU_MAX_TEST = 20
 AU_F_FAIL = 1
 AU_F_SIZE = 4
 
-stat_t_size = 1 + 10
-stat_fmt_t_size = 2 + 2
+sizeof_stat_t_size = 2
+sizeof_record_t = 2
 
 def help():
     print("USAGE: " + sys.argv[0] + " <file>")
 
-def au_unpack(t_size, fmt, addr, flag_count):
-    res = []
-    for i in range(flag_count):
+def au_unpack(record_size, t_size, fmt, addr, flag_count):
+
+    res =  [ [0] for i in range(flag_count) ]
+
+    for i in range(record_size):
         start = addr
         end = start + t_size
-        d = struct.unpack(fmt, data[start:end])
-        res.append(d)
+        flag, id_test = struct.unpack(fmt, data[start:end])
+        res[flag][0] += 1
+        res[flag].append(id_test)
         addr += t_size
     return res
 
-def print_stat(flag_count):
+def print_record(flag_count):
     for flag in range(flag_count):
 
-        flag_label = stat_fmt[flag][0].decode()
-        flag_data = str(stat[flag][0])
+        flag_label = record_fmt[flag][0]
+        flag_data = str(record[flag][0])
 
-        id_label = stat_fmt[flag][1].decode()
-        id_data = [hex(i) for i in stat[flag][1:] if (i)]
+        id_label = record_fmt[flag][1]
+        id_data = [hex(i) for i in record[flag][1:] if (i)]
         id_data = ", ".join(id_data)
 
         print(flag_label+":"+flag_data+"\t"+id_label+":"+id_data)
 
-stat = []
-stat_fmt = []
+record = []
+record_fmt = [
+        ['P ','ID'],
+        ['F ','ID'],
+        ['B ','ID'],
+        ['I ','ID']
+        ]
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        help()
-    with open(sys.argv[1], 'rb') as f:
-        data = f.read()
+    parser = argparse.ArgumentParser(
+            description="Dump avrunit result from EEPROM or Serial")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-f", "--file", help="EEPROM dump file")
+    group.add_argument("-p", "--port", help="Serial port")
+    parser.add_argument("-b", "--baud", type=int, help="Serial baud rate")
+    args = parser.parse_args()
+    if (args.port and args.baud is None):
+        parser.error('Serial port requires --baud argument')
 
-    #unpack stat_t
-    stat = au_unpack(stat_t_size, 'B10B', 0, AU_F_SIZE)
+    if (args.file):
+        with open(args.file, 'rb') as f:
+            data = f.read()
+    elif (args.port):
+        s = serial.Serial(args.port, args.baud)
 
-    #unpack stat_fmt
-    addr = stat_t_size * AU_F_SIZE
-    stat_fmt = au_unpack(stat_fmt_t_size, '2s2s', addr, AU_F_SIZE)
+        time.sleep(2)
+        s.write(b'r')
 
-    print_stat(AU_F_SIZE)
+        data = s.read(sizeof_stat_t_size + (sizeof_record_t*AU_MAX_TEST))
 
-    sys.exit(stat[AU_F_FAIL][0])
+    record_size = struct.unpack('H', data[0:sizeof_stat_t_size])[0]
+    record = au_unpack(record_size, sizeof_record_t, 'BB', 2, AU_F_SIZE)
+
+    print_record(AU_F_SIZE)
+
+    sys.exit(record[AU_F_FAIL][0])
--- tests/include/avrunit.h
+++ tests/include/avrunit.h
@@ -11,7 +11,11 @@
 #include <avr/io.h>
 #include <avr/eeprom.h>
 
-#define AU_MAX_TEST 10
+#ifndef AU_EEPROM_START
+#define AU_EEPROM_START 0
+#endif
+
+#define AU_MAX_TEST 20
 #define AU_F_PASS 0
 #define AU_F_FAIL 1
 #define AU_F_BROKEN 2
@@ -19,31 +23,85 @@
 #define AU_F_SIZE 4
 
 typedef struct {
-    uint8_t index;
-    uint8_t id[AU_MAX_TEST];
-} stat_t;
-
-stat_t AU_STAT[AU_F_SIZE];
+    uint8_t flag;
+    uint8_t id;
+} record_t;
 
 typedef struct {
-    char label_index[2];
-    char label_id[2];
-} stat_fmt_t;
+    uint16_t size;
+    record_t record[AU_MAX_TEST];
+} stat_t;
 
-stat_fmt_t AU_STAT_FMT[AU_F_SIZE] = {
-    {"P ", "ID"},
-    {"F ", "ID"},
-    {"B ", "ID"},
-    {"I ", "ID"}
-};
+stat_t AU_STAT;
+
+#ifdef AU_SERIAL
+#include <util/setbaud.h>
+
+#define USART_txReady()       (bit_is_set(UCSR0A, UDRE0))
+#define USART_rxReady()       (bit_is_set(UCSR0A, RXC0))
+#define USART_txWait()        ({loop_until_bit_is_set(UCSR0A, UDRE0);})
+#define USART_rxWait()        ({loop_until_bit_is_set(UCSR0A, RXC0);})
+#define USART_txByte(d)       (UDR0 = d)
+#define USART_rxByte()        (UDR0)
+
+static void USART_init(void) {
+  UBRR0H = UBRRH_VALUE;         // defined in setbaud.h & requires BAUD
+  UBRR0L = UBRRL_VALUE;
+#if USE_2X
+  UCSR0A |= _BV(U2X0);
+#else
+  UCSR0A &= ~_BV(U2X0);
+#endif
+
+  UCSR0B = _BV(TXEN0) | _BV(RXEN0);     // Enable USART transmitter/receiver
+  UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);   // 8 data bits, 1 stop bit
+}
+
+static void au_wait_serial(uint8_t cmd) {
+    uint8_t c;
+    while (c != cmd) {
+        USART_rxWait();
+        c = USART_rxByte();
+    }
+}
+
+static void au_send_serial(uint8_t *data, uint16_t size) {
+    uint8_t *byte;
+    for (byte = data; size > 0 ; size--, ++byte) {
+        USART_txWait();
+        USART_txByte(*byte);
+    }
+}
+
+static void au_output_serial(void) {
+    USART_init();
+    au_wait_serial('r');
+    au_send_serial((uint8_t *)&AU_STAT, sizeof(AU_STAT));
+
+}
+#endif // AU_SERIAL
+
+#ifndef AU_SERIAL
+static void au_output_eeprom(void) {
+    int addr = AU_EEPROM_START;
+    eeprom_write_block(&AU_STAT, (void *)addr, sizeof(AU_STAT));
+}
+#endif
+
+static void au_output(void) {
+
+#ifndef AU_SERIAL
+    au_output_eeprom();
+#endif
+
+#ifdef AU_SERIAL
+    au_output_serial();
+#endif
+
+}
 
 #define AU_OUTPUT_SETUP()
-#define AU_OUTPUT() do { \
-    int addr = 0; \
-    eeprom_write_block(&AU_STAT, (void *)addr, sizeof(AU_STAT)); \
-    addr = sizeof(AU_STAT); \
-    eeprom_write_block(&AU_STAT_FMT, (void *)addr, sizeof(AU_STAT_FMT)); \
-} while (0)
+#define AU_OUTPUT() au_output()
 #define AU_ASSERT(TEST) do { if (!(TEST)) { return AU_F_FAIL; } } while (0)
 #define AU_BROKEN(TEST) do { return AU_F_BROKEN; } while (0)
 #define AU_IGNORED(TEST) do { return AU_F_IGNORE; } while (0)
@@ -52,11 +110,10 @@ stat_fmt_t AU_STAT_FMT[AU_F_SIZE] = {
 #define AU_UNIT_END do { return AU_F_PASS; } while(0)
 
 #define AU_RUN_TEST(ID, TEST_CASE, ...) do { \
-    int flag, index; \
-    flag = TEST_CASE(__VA_ARGS__); \
-    index = AU_STAT[flag].index; \
-    AU_STAT[flag].id[index] = ID; \
-    AU_STAT[flag].index = index + 1; \
+    int index = AU_STAT.size; \
+    AU_STAT.record[index].flag = TEST_CASE(__VA_ARGS__); \
+    AU_STAT.record[index].id = ID; \
+    AU_STAT.size += 1; \
 } while (0)
 
 #endif // _AVRUNIT_H
