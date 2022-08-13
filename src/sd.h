5ca1ac145aa48631d502d8f024c8277007e8df1a
Khairulmizam Samsudin <xource@gmail.com>
2022-08-13 21:56:12 +0800

    Refactor SD_PIN_VCC macro definition

--- src/sd.h
+++ src/sd.h
@@ -2,11 +2,12 @@
 #define _SD_H
 #include "spi_driver.h"
 
-#define SD_PIN_VCC PINB1
+//#define SD_PIN_VCC PINB1
+
+#ifdef SD_PIN_VCC
 #define SD_DDR     DDRB
 #define SD_PORT    PORTB
 
-#ifdef SD_PIN_VCC
 #define SD_ENABLE()     (SD_PORT |= _BV(SD_PIN_VCC))
 #define SD_DISABLE()    (SD_PORT &= ~_BV(SD_PIN_VCC))
 #endif // SD_PIN_VCC
