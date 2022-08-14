ca4d3b200ee3ae7506202eec846dc0f0e0e47d94
Khairulmizam Samsudin <xource@gmail.com>
2022-08-14 16:33:41 +0800

    Add copyright notice and license

--- src/spi_driver.h
+++ src/spi_driver.h
@@ -1,3 +1,22 @@
+/* rwSD Library
+   Copyright (C) 2022 Khairulmizam Samsudin <xource@gmail.com
+
+   This file is part of the rwSD Library
+
+   This Library is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with the rwSD Library. If not, see
+   <http://www.gnu.org/licenses/>.
+*/
 #ifndef _SPI_H
 #define _SPI_H
 #include <avr/io.h>
